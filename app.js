8b16fd61f23fd9d05dfa4e7af3648554a2dd1864
SamiraMiss <samirarostami71@gmail.com>
2023-02-05 18:27:18 +0330

    added some changes

---
+++ app.js
@@ -0,0 +1,41 @@
+/*--------------------------------------------------------------------
+---------------------******intersectionObserver****-------------------
+----------------------------------------------------------------------
+ */
+
+const sections = document.querySelectorAll("section");
+const trans = document.querySelector(".trans");
+const gradients = ["coral", "chartreuse", "chocolate", "cadetblue"];
+
+const options = {
+  threshold: 0.7,
+};
+
+let observer = new IntersectionObserver(navScroll, options);
+
+function navScroll(entries) {
+  entries.forEach(function (entry) {
+    const className = entry.target.className;
+    const activeLink = document.querySelector(`[data-page="${className}"]`);
+    const elementIndex = entry.target.getAttribute("data-index");
+    const coordinates = activeLink.getBoundingClientRect();
+    const directions = {
+      width: coordinates.width,
+      height: coordinates.height,
+      top: coordinates.top,
+      left: coordinates.left,
+    };
+
+    if (entry.Intersecting) {
+      trans.style.setProperty("height", `${directions.height}px`);
+      trans.style.setProperty("width", `${directions.width}px`);
+      trans.style.setProperty("top", `${directions.top}px`);
+      trans.style.setProperty("left", `${directions.left}px`);
+      trans.style.backgroundColor = gradients[elementIndex];
+    }
+  });
+}
+
+sections.forEach(function (section) {
+  observer.observe(section);
+});
